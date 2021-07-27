0abf6b41852a68447b4c48bfdbe3f2e1b8a18caa
omer <gs_omer_ozkan@hotmail.com>
2021-07-27 11:10:30 +0300

    Add project files.

---
+++ OmerOzkan.ToDo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OmerOzkan.ToDo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
